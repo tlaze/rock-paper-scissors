bcba90dc51cbf9058e7ec34c4e808181cc02c76a
Tom Lazore <m>
2021-09-16 12:19:05 -0400

    Added popups if user cancels mid game

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ function playGame(){
         else{
             alert("Lets play soon! Refresh the page when you are ready.");
         }
-
+}
 //Prompts user for choices and determines who won that round
 function gamePlay(){
 
         // Prompts user for choice
         var userChoice = prompt("Type R for Rock, P for Paper, or S for Scissors");
-
 
         // Action if user chooses Rock
         if(userChoice === "R"|| userChoice === "r" || userChoice === "Rock" || userChoice === "rock"){
@@ -34,12 +33,22 @@ function gamePlay(){
         else if(userChoice === "S"|| userChoice === "s" || userChoice === "Scissors" || userChoice === "scissors"){
             console.log("User chose Scissors");
         }
+        else if(userChoice === null){
+            var endEarly = confirm("Are you sure you want to end the game?\nClick 'Ok' to End or 'Cancel' to Return to the Game");
+            console.log(endEarly);
+            if(endEarly == true){
+                alert("Thanks For Playing!");
+            }
+            else{
+                gamePlay();
+            }
+        }
         // Action if user doesn't enter appropriate response
         else{
             alert("Please chose either Rock, Paper, or Scissors");
+            console.log(userChoice);
             gamePlay();
         }
-    }
 }
 
 playGame();
