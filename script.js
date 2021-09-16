10f098d48108a5e8f8abb66cb79217df2277e584
Tom Lazore <m>
2021-09-16 12:00:37 -0400

    added prompts for each choice user can make

---
+++ script.js
@@ -0,0 +1,45 @@
+var wins = 0;
+var losses = 0
+var ties = 0;
+
+
+// Initiates Game
+function playGame(){
+    var letsPlay = confirm("Lets play Rock, Paper, Scissors");
+        // If user hits Ok then game starts and user is prompted with choice
+        if(letsPlay === true){
+            gamePlay();
+        }
+        // If the user declines to play
+        else{
+            alert("Lets play soon! Refresh the page when you are ready.");
+        }
+
+//Prompts user for choices and determines who won that round
+function gamePlay(){
+
+        // Prompts user for choice
+        var userChoice = prompt("Type R for Rock, P for Paper, or S for Scissors");
+
+
+        // Action if user chooses Rock
+        if(userChoice === "R"|| userChoice === "r" || userChoice === "Rock" || userChoice === "rock"){
+            console.log("User chose Rock");
+        }
+        // Action if user chooses Paper
+        else if(userChoice === "P"|| userChoice === "p" || userChoice === "Paper" || userChoice === "paper"){
+            console.log("User chose Paper");
+        }
+        // Action if user choses Scissors
+        else if(userChoice === "S"|| userChoice === "s" || userChoice === "Scissors" || userChoice === "scissors"){
+            console.log("User chose Scissors");
+        }
+        // Action if user doesn't enter appropriate response
+        else{
+            alert("Please chose either Rock, Paper, or Scissors");
+            gamePlay();
+        }
+    }
+}
+
+playGame();
